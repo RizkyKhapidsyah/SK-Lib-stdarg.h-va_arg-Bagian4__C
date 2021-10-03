ebc50f177a26f983a15683a1e39b2f7429f60871
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-10-03 07:23:29 +0700

    Add project files.

---
+++ SK-Lib-stdarg.h-va_arg-Bagian4__C/Source.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
+#include <conio.h>
+
+/*
+    Source by CPPReference
+    Modified For Learn by RK
+    I.D.E : VS2019
+*/
+
+double stddev(int count, ...) {
+    double sum = 0;
+    double sum_sq = 0;
+    va_list args;
+    va_start(args, count);
+
+    for (int i = 0; i < count; ++i) {
+        double num = va_arg(args, double);
+        sum += num;
+        sum_sq += num * num;
+    }
+    
+    va_end(args);
+    return sqrt(sum_sq / count - (sum / count) * (sum / count));
+}
+
+int main(void) {
+    printf("%f\n", stddev(4, 25.0, 27.3, 26.9, 25.7));
+    _getch();
+    return 0;
+}
